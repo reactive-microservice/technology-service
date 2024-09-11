2d2b2917983547787a7d257978011a3d6a701295
kennedygamarra <kennedygamarrad@gmail.com>
2024-09-11 16:08:06 -0500

    feat: technology save service + unit test

---
+++ src/test/java/com/pragma/technology_service/TechnologyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.technology_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
